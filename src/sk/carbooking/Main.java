0793e6a08d77c8b640d3b67f9ab7cf2d95f6dcfa
vitalij-demjanovic <vitalijdemjanovic@gmail.com>
2023-06-29 22:14:44 +0200

    create displayMenu & choice with scanner

--- src/sk/carbooking/Main.java
+++ src/sk/carbooking/Main.java
@@ -2,11 +2,46 @@ package sk.carbooking;
 
 import sk.carbooking.user.UserService;
 
-import java.util.UUID;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         UserService userService = new UserService();
-        System.out.println(userService.gerUserById(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3")));
+
+        boolean keepLooping = true;
+
+        try {
+            while (keepLooping) {
+                displayMenu();
+                String choice = scanner.nextLine();
+                switch (Integer.parseInt(choice)) {
+                    case 1 -> System.out.println("choice 1");
+                    case 2 -> System.out.println("choice 1");
+                    case 3 -> System.out.println("choice 1");
+                    case 4 -> System.out.println("choice 1");
+                    case 5 -> System.out.println("choice 1");
+                    case 6 -> System.out.println(Arrays.toString(userService.getUsers()));
+                    case 7 -> keepLooping = false;
+                    default -> System.out.println(choice + " not a valid option ❌");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    private static void displayMenu() {
+        System.out.println("""
+                \n
+                1️⃣ - Book Car
+                2️⃣ - View All User Booked Cars
+                3️⃣ - View All Bookings
+                4️⃣ - View Available Cars
+                5️⃣ - View Available Electric Cars
+                6️⃣ - View all users
+                7️⃣ - Exit
+                """);
     }
 }
