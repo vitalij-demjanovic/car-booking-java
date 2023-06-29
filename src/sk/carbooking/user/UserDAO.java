363863142f7f77c0e4535b858a326af7dd5b395e
vitalij-demjanovic <vitalijdemjanovic@gmail.com>
2023-06-29 15:24:34 +0200

    create UserDAO

---
+++ src/sk/carbooking/user/UserDAO.java
@@ -0,0 +1,19 @@
+package sk.carbooking.user;
+
+import java.util.UUID;
+
+public class UserDAO {
+    private static final User[] users;
+
+    static {
+        users = new User[]{
+                new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "Jano"),
+                new User(UUID.fromString("b10d126a-3608-4980-9f9c-aa179f5cebc3"), "Gabriela")
+        };
+    }
+
+
+    public User[] getUsers() {
+        return users;
+    }
+}
